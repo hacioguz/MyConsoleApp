f81407e2e0fba53e5cb7c4ef2f0708ab279947a5
Software Developer <sware.developer@gmail.com>
2020-01-13 03:21:06 +0300

    third commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HaciOguz.MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
